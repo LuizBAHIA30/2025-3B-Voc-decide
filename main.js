c4139d0e12f803ba7fcb70cbdb82ac7da15be0e3
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2025-06-30 08:00:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,82 @@
-const caixaPrincipal = document.querySelector(".caixa-Principal");
-const caixaPerguntas = document.querySelector("caixa-Perguntas");
-const caixaAlternativas = document.querySelector("caixa.Alternativas");
-const caixaResultados = document.querySelector("caixa-Resultados");
-const textoResultos = document.querySelector("texto-Resultados");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const Perguntas = [
-{
-    enuciado:"perguntas 1",
-    alternatias:[
-        "alternativa 1",
-        "alternativa 2"
-    ]
-},
-{
-    enuciado:"perguntas 2",
-    alternatias:[
-        "alternativa 3",
-        "alternativa 4"
-    ]
-},
-{
-    enuciado:"perguntas 3",
-    alternatias:[
-        "alternativa 5",
-        "alternativa 6"
-    ]
-}
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: "resultado 1"
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: "resultado 2"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "alternativa 3",
+                afirmacao: "resultado 3"
+            },
+            {
+                texto: "alternativa 4",
+                afirmacao: "resultado 4"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "alternativa 5",
+                afirmacao: "resultado 5"
+            },
+            {
+                texto: "alternativa 6",
+                afirmacao: "resultado 6"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
+        ]
+    }
 ]
-
-let atual =0;
-let PerguntaAtual;
-
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 function mostraPergunta(){
-    PerguntaAtual = Perguntas[atual]
-    caixaPerguntas.textContent = PerguntaAtual.enuciado
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+    mostraAlternativas()
 }
-
-console.log(caixaPrincipal)
+function mostraAlternativas(){
+    for( const alternativa of perguntaAtual.alternativas ){
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    atual++
+    mostraPergunta();
+}
+mostraPergunta();
+console.log(perguntas)
